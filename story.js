8aac1ebae13c3be594182ef41f0df335c2544a67
Ali-Altabtabae <78157385+Ali-Altabtabae@users.noreply.github.com>
2021-02-01 19:46:26 +0300

    Task 3

---
+++ story.js
@@ -0,0 +1,7 @@
+let name = "Ali";
+let age = 23;
+let hobby = "soccer";
+
+console.log(`
+Mr./Mrs. ${name}, in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hobby} practice!
+`);
